5f696ed18fd137fe9d3f1c1ce2947f982d817c3e
Md Monerujamman Munna <mdmonerujammanmunna@gmail.com>
2026-06-01 22:35:51 +0600

    Profile section design: :

--- src/app/profilePage/page.jsx
+++ src/app/profilePage/page.jsx
@@ -2,9 +2,17 @@
 
 const profilePage = () => {
     return (
-        <div>
-            profile pagee
-        </div>
+        <>
+            <div className="py-20 px-10">
+                <div className="mb-10">
+                    <h1 className="text-6xl font-bold">My Profile</h1>
+                    <p className="text-xl mt-4 text-[#6C696D]">Manage your account settings and travel preferences</p>
+                </div>
+                <div className="">
+                    <h1 className="text-2xl font-semibold">Travel Statistics</h1>
+                </div>
+            </div>
+        </>
     );
 };
 
